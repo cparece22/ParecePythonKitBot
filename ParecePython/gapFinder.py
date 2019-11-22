10437fe8ac0a14e22741286a512f5d861e6d6c93
cparece22 <53668869+cparece22@users.noreply.github.com>
2019-11-22 13:43:10 -0600

    Update gapFinder.py

--- ParecePython/gapFinder.py
+++ ParecePython/gapFinder.py
@@ -2,32 +2,35 @@ import RoboPiLib as RPL
 import setup
 import time
 from time import sleep
+sensor_pinM = 4
 sensor_pinF = 5
 sensor_pinL = 6
 sensor_pinR = 7
 RPL.pinMode(sensor_pinF, RPL.INPUT)
 RPL.pinMode(sensor_pinL, RPL.INPUT)
 RPL.pinMode(sensor_pinR, RPL.INPUT)
+RPL.pinMode(sensor_pinM, RPL.INPUT)
 R = 1
 L = 0
 while True:
     currentDistanceForward = RPL.analogRead(sensor_pinF)
     currentDistanceLeft = RPL.analogRead(sensor_pinL)
     currentDistanceRight = RPL.analogRead(sensor_pinR)
-    if currentDistanceLeft <= 10:
-        RPL.servoWrite(L,0)
-        RPL.servoWrite(R,0)
-        if currentDistanceForward >= 100:
+    currentDistanceSpec = RPL.analogRead(sensor_pinM)
+    if currentDistanceLeft >= 50:
+        if 350 < currentDistanceLeft < 400:
+            RPL.servoWrite(L,1400)
+            RPL.servoWrite(R,1600)
+        elif currentDistanceLeft <= 350:
             RPL.servoWrite(L,1450)
             RPL.servoWrite(R,1600)
-            sleep(0.5)
-            print(currentDistanceForward)
-            break
-    elif currentDistanceLeft >= 315:
-        RPL.servoWrite(L,1400)
-        RPL.servoWrite(R,1550)
-    elif currentDistanceLeft >= 315:
-        RPL.servoWrite(L,1450)
-        RPL.servoWrite(R,1600)
-    
-        
+        elif currentDistanceLeft >= 400:
+            RPL.servoWrite(L,1400)
+            RPL.servoWrite(R,1550)
+    elif currentDistanceLeft <= 50:
+        if currentDistanceSpec >= 50:
+            RPL.servoWrite(L,1400)
+            RPL.servoWrite(R,1600)
+        elif currentDistanceSpec <= 50:
+            RPL.servoWrite(L,1400)
+            RPL.servoWrite(R,1550)
